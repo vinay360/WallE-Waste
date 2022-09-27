4facef0e491c29980fbf3a53c6620906d6843731
vinay360 <97460542+vinay360@users.noreply.github.com>
2022-09-28 00:03:12 +0530

    Update twilio.js

--- twilio.js
+++ twilio.js
@@ -1,6 +1,6 @@
 // // const twilio = require('twilio');
-const accountSid = 'AC35c62cfe81eeda9388f8f1675a9ccb15'; // Your Account SID from www.twilio.com/console
-const authToken = '72e33326cfa87c88f3cc1a4cc0946148'; // Your Auth Token from www.twilio.com/console
+const accountSid = '########################'; // Your Account SID from www.twilio.com/console
+const authToken = '######################'; // Your Auth Token from www.twilio.com/console
 
 // const twilio = require('twilio');
 // const client = new twilio(accountSid, authToken);
@@ -9,14 +9,14 @@ const authToken = '72e33326cfa87c88f3cc1a4cc0946148'; // Your Auth Token from ww
 //   .create({
 //     body: 'Hello your otp for order id 69696 is 69696!',
 //     to: '+917851875570', // Text this number
-//     from: '+19377779515', // From a valid Twilio number
+//     from: '+18454796532', // From a valid Twilio number
 //   })
 //   .then((message) => console.log(message.sid));
 
 function sendOtp(mobileNo,otp,orderID){
     const client = require('twilio')(accountSid, authToken);
     client.messages
-    .create({body: `Hi, OTP for order id ${orderID}  is ${otp}!`, from: '+19377779515', to: `${mobileNo}`})
+    .create({body: `Hi, OTP for order id ${orderID}  is ${otp}!`, from: '+18454796532', to: `+917851875570`})
     .then(message => console.log(message.sid));
 }
 
